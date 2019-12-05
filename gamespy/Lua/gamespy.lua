9414e0909a90602c1533c76091b414878543e9c4
Daniel Ornelas <dornelas4@miners.utep.edu>
2019-12-05 15:05:25 -0700

    gamespy:

--- gamespy/Lua/gamespy.lua
+++ gamespy/Lua/gamespy.lua
@@ -2,7 +2,16 @@
 protocol = Proto('gamespy','Gamespy protocol' )
 
  -- Declaration of fields
- local tab = {  -- tab[i][j] = xor(i-1, j-1)
+ 
+
+ -- Adding fields to protocol fields attribute 
+protocol.fields ={ } 
+
+ -- Dissector function 
+function protocol.dissector(buffer,pinfo,tree) 
+	 pinfo.cols.protocol = protocol.name 
+	 local subtree = tree:add(protocol, buffer(), "gamespy" )
+ 	 local tab = {  -- tab[i][j] = xor(i-1, j-1)
   {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, },
   {1, 0, 3, 2, 5, 4, 7, 6, 9, 8, 11, 10, 13, 12, 15, 14, },
   {2, 3, 0, 1, 6, 7, 4, 5, 10, 11, 8, 9, 14, 15, 12, 13, },
@@ -20,50 +29,41 @@ protocol = Proto('gamespy','Gamespy protocol' )
   {14, 15, 12, 13, 10, 11, 8, 9, 6, 7, 4, 5, 2, 3, 0, 1, },
   {15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, },
 }
-
- -- Adding fields to protocol fields attribute 
-protocol.fields ={ } 
-
- -- Dissector function 
-function protocol.dissector(buffer,pinfo,tree) 
-	 pinfo.cols.protocol = protocol.name 
-	 local subtree = tree:add(protocol, buffer(), "udp" )
- cipher = {103, 97, 109, 101, 115, 112, 121}
-index = 1
-decoded = ""
+cipher = {103, 97, 109, 101, 115, 112, 121}
 size = buffer:len()
- 		 for i=0, size-1, 1 
- 	 do 
- 	local number res = 0 
-local number c = 0 
-local number a = 0 
-local number b = 0 
-a = buffer(i,1):uint()
+ 	 local number index = 1 
+	 local string decoded = "" 
+	  for i=0, size-1, 1 do 
+ 	 	 local number res = 0 
+	 	 local number c = 0 
+	 	 local number a = 0 
+	 	 local number b = 0 
+	 	 a = buffer(i,1):uint()
 b = cipher[index]
- 		 while(a > 0)
+ while(a > 0)
  do 
- 	local number a2 = 0 
-local number b2 = 0 
-a2 = a%16 
+ 	 	 	 local number a2 = 0 
+	 	 	 local number b2 = 0 
+	 	 	 a2 = a%16
 b2 = b%16
- 	res = res + tab[a2+1][b2+1]*c
+ 	 	 	 res = res + tab[a2+1][b2+1]*c
   a = (a-a2)/16
   b = (b-b2)/16
   c = c*16
- 		 end 
- 	 res = res + a*c + b*c
- 	decoded = decoded .. string.char(res)
+ 	 	 end 
+  	 	 	 res = res + a*c + b*c
+ 	 	 	 decoded = decoded .. string.char(res)
 index = index + 1
 
- 		 if index == 8 then 
- 	index = 1
- 		 else 
- 		 end 
- 	
-	 end 
- 	 local subtree = tree:add(protocol,buffer(),"Gamspy Protocol")
+ 	 	 	 if index == 8 then 
+ 	 	 	 	 index = 1
+ 	 	 	 else 
+ 	 	 	 end 
+ 	 	 
+	  end 
+ 	 	 local subtree = tree:add(protocol,buffer(),"Gamspy Protocol")
 subtree:add(buffer(0,size), "Decoded: " .. decoded)
- 	
+ 
 end 
  
 local port = DissectorTable.get("tcp.port") 
